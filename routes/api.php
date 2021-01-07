4116e9ff855a44d006a1f4c8ad5b7717345d721d
Gheorghe Ribalcenco <76951753+gicu170587@users.noreply.github.com>
2021-01-07 21:26:13 +0000

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
+
+Route::group(['prefix' => 'auth'], function ($router) {
+
+    Route::post('register', 'App\Http\Controllers\AuthController@register'); 
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+  
+  });
+
+Route::get('items/user/{id}',  'App\Http\Controllers\ItemController@index');
+Route::prefix('/item')->group( function(){
+    Route::post('/store','App\Http\Controllers\ItemController@store');
+    Route::put('/{id}', 'App\Http\Controllers\ItemController@update');
+    Route::get('/{id}', 'App\Http\Controllers\ItemController@show');
+    Route::delete('/{id}', 'App\Http\Controllers\ItemController@destroy');
+});
---
+++ storage/framework/views/aca48de90daf3f78f7c77cc322252bb2e374b279.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="grweb">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <title>ToDO app</title>
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+        <main-App />
+    </div>
+    <script src="<?php echo e(mix('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\Users\Admin\todoApp\resources\views/welcome.blade.php ENDPATH**/ ?>
