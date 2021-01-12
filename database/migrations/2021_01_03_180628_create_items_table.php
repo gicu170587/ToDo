0f1457df2f2868b4d94a492430ddefa6da58fba4
Gheorghe Ribalcenco <76951753+gicu170587@users.noreply.github.com>
2021-01-12 19:45:35 +0000

    Add files via upload

---
+++ database/migrations/2021_01_03_180628_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('name');
+            $table->text('desc');
+            $table->integer('completed')->default(false);
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
